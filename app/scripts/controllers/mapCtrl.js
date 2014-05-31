d3944bba4075f8aa62e309935e2e961b9122a153
carlyleec <carlyleec@gmail.com>
2014-05-31 16:00:02 -0400

    comment

--- app/scripts/controllers/mapCtrl.js
+++ app/scripts/controllers/mapCtrl.js
@@ -1,4 +1,4 @@
-angular.module('trunkApp').controller('mapCtrl', ['$scope', '$http',
+angular.module('trunkApp').controller('mapCtrl', ['$scope', '$http', '$q',
   function($scope, $http){
         	var modeMapper = new MapMyMode.Map();
 			modeMapper.initialize('map');
@@ -14,12 +14,49 @@ angular.module('trunkApp').controller('mapCtrl', ['$scope', '$http',
 			params.sensor = false;
 			//always true 
 			params.alternatives = true;
-			modeMapper.getRoute();
+			
+			var getRoute = function(orgin, destination, travelType){
+				var travelModeObj = {
+					"walking" : google.maps.TravelMode.TRANSIT,
+					"bicycling" : google.maps.TravelMode.BICYCLING,
+					"driving" : google.maps.TravelMode.DRIVING,
+					"transit" : google.maps.TravelMode.TRANSIT
+				};
+				var directionsService = new google.maps.DirectionsService();
+				var directionsDisplay = new google.maps.DirectionsRenderer();
+				directionsDisplay.setMap(modeMapper.map);
+				 var request = {
+				      origin: orgin,
+				      destination: destination,
+				      travelMode: travelModeObj[travelType]
+				  };
+				  directionsService.route(request, function(response, status) {
+				    if (status == google.maps.DirectionsStatus.OK) {
+				      directionsDisplay.setDirections(response);
+				      for (var i = 0; i < response.routes[0].legs[0].steps.length; i++) {
+							console.log(response.routes[0].legs[0].steps[i].instructions);
+							console.log(response.routes[0].legs[0].steps[i].travel_mode);
+							console.log(response.routes[0].legs[0].steps[i].path);
+						};
+				      
+				    }
+				  });
+			}
+
+			//
+			getRoute('25 Howland Rd Asheville NC', '43 Wall Street Asheville NC', 'walking');
+			var annotationCallback = function(data){
+				console.log(data)
+			}
+			modeMapper.addDrawingTool("point", annotationCallback);
+			// console.log(route);
+			// for (var i = 0; i < route.routes[0].legs[0].steps.length; i++) {
+			// 	console.log(route.routes[0].legs[0].steps[i].instructions)
+			// };
 
 			
 
 }]);
-
 
 
 
